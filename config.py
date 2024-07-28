b99dc3e9ceb4c7eb620e385b5c7d061d9e2f79d0
minhpc <76472728+wiuy3120@users.noreply.github.com>
2024-07-29 01:06:20 +0700

    feat: init database

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "env_secret_key")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL", "sqlite:///" + os.path.join(basedir, "app.db")
+    )
