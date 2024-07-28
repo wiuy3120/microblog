258f7bede6aff0b862ddccab35db6f35f7ad233b
minhpc <76472728+wiuy3120@users.noreply.github.com>
2024-07-28 16:35:41 +0700

    feat: create and apply base template

--- src/routes.py
+++ src/routes.py
@@ -6,4 +6,8 @@ from flask import render_template
 @app.route("/index")
 def index():
     user = {"user_name": "Peri"}
-    return render_template("index.html", title="Home Page", user=user)
+    posts = [
+        {"author": {"user_name": "Yoru"}, "body": "Never show weakness. Ever."},
+        {"author": {"user_name": "Jhin"}, "body": "The stage is set."},
+    ]
+    return render_template("index.html", title="Home Page", user=user, posts=posts)
