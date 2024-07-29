e63ba39178963c784f22df28492bc72bd3af8df8
minhpc <76472728+wiuy3120@users.noreply.github.com>
2024-07-30 00:11:58 +0700

    chore: add flask shell configs

--- microblog.py
+++ microblog.py
@@ -1,1 +1,9 @@
-from src import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from src import app, db
+from src.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"sa": sa, "so": so, "db": db, "User": User, "Post": Post}
